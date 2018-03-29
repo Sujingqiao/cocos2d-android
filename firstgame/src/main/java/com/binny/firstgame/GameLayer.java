ece3575497e8c254a1d4d0c3c8de1da10fbe97d6
Xbean1024 <596928539@qq.com>
2018-03-29 16:04:42 +0800

    精灵初体验

--- firstgame/src/main/java/com/binny/firstgame/GameLayer.java
+++ firstgame/src/main/java/com/binny/firstgame/GameLayer.java
@@ -1,6 +1,10 @@
 package com.binny.firstgame;
 
+import org.cocos2d.actions.interval.CCJumpTo;
 import org.cocos2d.layers.CCLayer;
+import org.cocos2d.nodes.CCSprite;
+import org.cocos2d.types.CCPointSize;
+import org.cocos2d.types.CGPoint;
 
 /**
  * author  binny
@@ -8,6 +12,29 @@ import org.cocos2d.layers.CCLayer;
  * 布景层
  */
 public class GameLayer extends CCLayer{
+    private CCSprite mSprite;
     public GameLayer() {
+        /*
+        * 添加精灵对象
+        * */
+        mSprite = CCSprite.sprite("player.png");
+        /*
+        * 设置精灵对象的位置
+        * */
+        CGPoint point = CGPoint.ccp(100,100);
+        mSprite.setPosition(point);
+
+        /*
+        * 将该对象 mSprite 添加到布景层
+        * */
+        this.addChild(mSprite);
+        /*
+        * 生成jumpTo对象
+        * */
+        CCJumpTo jumpTo = CCJumpTo.action(3,CGPoint.ccp(500,100),200,5);
+        /*
+        * 使用精灵对象执行该动作
+        * */
+        mSprite.runAction(jumpTo);
     }
 }
